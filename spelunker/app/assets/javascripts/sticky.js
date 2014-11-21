f442b289b0445d206b2a51d71a4defc3044c0cef
Tom Armitage <tom@infovore.org>
2014-11-21 15:15:16 +0000

    Working sticky header on viz.

--- spelunker/app/assets/javascripts/sticky.js
+++ spelunker/app/assets/javascripts/sticky.js
@@ -2,9 +2,9 @@ function sticky_relocate() {
     var window_top = $(window).scrollTop();
     var div_top = $('#sticky-anchor').offset().top;
     if (window_top > div_top) {
-        $('#sticky').addClass('stick');
+        $('.stickable').addClass('stick');
     } else {
-        $('#sticky').removeClass('stick');
+        $('.stickable').removeClass('stick');
     }
 }
 
