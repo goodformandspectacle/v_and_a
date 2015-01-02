4fa105353a6f454557390cfb0b4a878c41633101
Tom Armitage <tom@infovore.org>
2015-01-02 19:09:24 +0000

    Fixing sticky placement.

--- spelunker/app/assets/javascripts/sticky.js
+++ spelunker/app/assets/javascripts/sticky.js
@@ -1,14 +1,4 @@
-function sticky_relocate() {
-    var window_top = $(window).scrollTop();
-    var div_top = $('#sticky-anchor').offset().top;
-    if (window_top > div_top) {
-        $('.stickable').addClass('stick');
-    } else {
-        $('.stickable').removeClass('stick');
-    }
-}
-
 $(function () {
-    $(window).scroll(sticky_relocate);
-    sticky_relocate();
+  $(".stickable").sticky({topSpacing:0,
+                          getWidthFrom: '.container'});
 });
