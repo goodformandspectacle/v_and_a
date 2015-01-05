cac458ef068531f172ab539a8e400c8b78642a6e
Tom Armitage <tom@infovore.org>
2015-01-05 15:46:26 +0000

    Removing HTTP auth.

--- spelunker/app/controllers/application_controller.rb
+++ spelunker/app/controllers/application_controller.rb
@@ -3,8 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  if Rails.env.production?
-    http_basic_authenticate_with name: "gfs", password: "nicetoseeyou", except: :index
-  end
+  # simple HTTP Basic auth.
+  #if Rails.env.production?
+    #http_basic_authenticate_with name: "gfs", password: "nicetoseeyou", except: :index
+  #end
   
 end
