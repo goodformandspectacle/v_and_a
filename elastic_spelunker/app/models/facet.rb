e8611235df95b5df5f18e639d53a66c0c2bd78e9
Tom Armitage <tom@infovore.org>
2015-02-19 15:40:08 +0000

    object_type is the name of the field.

--- elastic_spelunker/app/models/facet.rb
+++ elastic_spelunker/app/models/facet.rb
@@ -1,6 +1,6 @@
 class Facet
   def self.all
-    %w{artist object materials_techniques materials techniques place location collection_code year_start year_end}
+    %w{artist object_type materials_techniques materials techniques place location collection_code year_start year_end}
   end
 
   def self.lists
